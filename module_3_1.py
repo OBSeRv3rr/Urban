fc059c1aa9f8be2d54c0bfe4e72f7238ca3f38e5
OBSeRv3rr <vlad.gud.vlad@mail.ru>
2024-11-22 16:59:08 +0300

    3_1

--- module_3_1.py
+++ module_3_1.py
@@ -9,6 +9,12 @@ def string_info(string):
 
 def is_contains(string, list_to_search):
     count_calls()
-    
+    string_low = string.lower()
+    list_low = [item.lower() for item in list_to_search]
+    return string_low in list_low
 
-print(string_info('Hello'))
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
